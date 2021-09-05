f9866f71d3f71a220ecda1ba77c40de3874f8e26
Roesn <Bubwub04@icloud.com>
2021-09-05 15:04:25 -0400

    ex02

--- src/main/java/Solution02.java
+++ src/main/java/Solution02.java
@@ -1,14 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
 import java.util.Scanner;
 
 public class Solution02 {
-    /* Create a program
+    /*
+    Create a program
     Scanner 'input' = new scanner for input string
     print 'What is the input string? '
     String 'str' = scan in input string using 'input' scanner
     int 'ch' = 0 to count num characters
     for(iterate through string to count characters, get length of string)
         'ch'++ for every character
-    print 'str' has 'ch' characters. ' */
+    print 'str' has 'ch' characters. '
+    */
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("What is the input string? ");
